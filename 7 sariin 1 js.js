2282cda33737384ee76fa70e5abc477baba5d6ac
themuuln <zerone.offical@gmail.com>
2022-07-01 19:21:07 +0800

    first commit

---
+++ 7 sariin 1 js.js
@@ -0,0 +1,140 @@
+/*
+console.log('Hello World!');
+let b = 4 + 4;
+console.log(b);
+*/
+
+/*
+let a = 1;
+let b = 2;
+
+sum = a + b;
+
+console.log(sum);
+*/
+
+/*
+let a, b, sum;
+a = 34;
+b = 65;
+sum = a + b;
+console.log(sum);
+*/
+
+/*
+let a = 10;
+let b = 5;
+let min; 
+
+min = a - b; 
+console.log(min);
+*/
+
+/*
+let a = 2;
+let b = 12;
+let multiply = a * b;
+console.log(multiply);
+*/
+
+/*
+let a = 50
+let b = 25
+let divide = a / b
+console.log(divide)
+*/
+
+/*
+let a = 9
+let b = 2 
+let c = a % b
+console.log(c)
+*/
+
+// Бодлого 1 А болон В тоо өгөгдсөн бол ихийг нь ол. А болон В тоо өгөгдсөн бол жиших үйлдэл хийлгүйгээр ихийг нь ол.
+
+/*
+let a = prompt('Enter A')
+let b = prompt('Enter B')
+let c = (abs(a + b)+abs(a - b)) / 2 
+console.log(c)
+*/
+
+// Гурвалжны 3 тал өгөгдсөн бол. Өгөгдсөн гурвалжны периметрийг ол.
+
+/*
+let a, b, c, p;
+a = 12;
+b = 12;
+c = 24;
+p = a + b + c;
+console.log(p)
+*/
+
+// y=3x-5. x-ийн дурын утгад y-ийн утгыг ол.
+
+/*
+let y, x = 2;
+
+y = 3 * x - 5; 
+
+console.log(y); // Prints 1
+*/
+
+// Өгөгдсөн тооны аравтын орны цифрийг ол.
+
+
+// let a = 24, f;
+// f = Math.floor(24 % 100 / 10)
+// console.log(f)
+
+
+// Өгөгдсөн 3 оронтой тооны цифрүүдийн нийлбэрийг ол.
+
+/*
+let a = 123;
+let b = 
+*/
+
+// Өгөгдсөн секундыг цаг минут секунд болго.
+
+
+// let s = 3670
+// let m = Math.floor(s / 60)
+// let h = Math.floor(s / 3600)
+// let h2 = Math.floor(h % 60)
+
+// console.log(h + "Hour(s)", m + " Minute", Math.floor(s % 60) + " Seconds")
+
+
+// Өгөгдсөн цагийг хоног цагт шилжүүл.
+
+
+// let hours = 76
+// let days = Math.floor(hours / 24)
+// let hours2 = Math.floor(hours % 24) 
+// console.log(days + "Udur", hours2 + "Tsag")
+
+
+// Бодлого 1 Дараах утгуудын өгөгдлийн төрлийг бичнэ үү. Ингэхдээ typeof командыг ашиглаж түүнийг console.log хийж олно. 
+
+// console.log(typeof(15));
+// console.log(typeof(42.7));
+// console.log(typeof(2.5e-4));
+// console.log(typeof(Infinity));
+// console.log(typeof(NaN));
+// console.log(typeof(''));
+// console.log(typeof('hello')); 
+// console.log(typeof('12'));
+// console.log(typeof(true));
+// console.log(typeof(false));
+// console.log(typeof(undefined));
+// console.log(typeof(undeclaredVariable));
+// console.log(typeof(null));
+
+// 1.1 my_num нэртэй number өгөгдлийн төрөлтэй хувьсагчид 3 гэсэн утгыг оноо.
+// let my_num = 3
+// 1.2 my_bool нэртэй boolean өгөгдлийн төрөлтэй хувьсагчид үнэн гэсэн утгыг оноо.
+// let my_bool = true
+// 1.3 my_str нэртэй string өгөгдлийн төрөлтэй хувьсагчид Nest neg zurh гэсэн утгыг оноо
+// let my_str 'Nest neg zurh'
